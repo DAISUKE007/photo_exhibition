150d911062d54aa1aeab038943dd0b9f0081cc5f
DAISUKE007 <da1suke.haseee@gmail.com>
2019-12-19 23:30:33 +0900

    削除機能の追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,10 +17,22 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
   end
 
+  def destroy
+    @photo = Photo.find(params[:id])
+    if user_signed_in? && @photo.user_id == current_user.id
+      @photo.destroy
+      redirect_to dComplete_photos_path
+    else
+      render acction: :show
+    end
+  end
+
+  def dComplete
+  end
+
   private
   def photo_params
     params.require(:photo).permit(:image, :title, :explanation).merge(user_id: current_user.id)
-    # .merge(user_id: current_user.id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "photos#index"
-  resources :photos, only: [:index, :new, :create, :show]
+  resources :photos, only: [:index, :new, :create, :show, :destroy] do
+    collection do
+      get 'dComplete'
+    end
+  end
 end
